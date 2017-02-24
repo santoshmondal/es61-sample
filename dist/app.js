3a155e593e4500fb415e6e6b622eaf6a46c3cbf2
santosh <santosh.ece06@gmail.com>
2017-02-24 13:48:03 +0530

    Initial commit

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * Created by santosh on 2/24/17.
+ */
+var firstName = "Affixus ";
+var middleName = "Systems";
+var lastName = " Pvt Ltd";
+
+console.log(firstName + " :: " + middleName);
---
+++ src/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by santosh on 2/24/17.
+ */
+let firstName = "Affixus ";
+let middleName = "Systems";
+let lastName = " Pvt Ltd";
+
+
+console.log(firstName + " :: " + middleName);
